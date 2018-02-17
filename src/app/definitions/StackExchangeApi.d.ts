69916d7cb6ef3b8e5d4fa4c51e554041bfbd8d4a
TheLox95 <leo_9_t@hotmail.com>
2018-02-16 21:07:06 -0400

    +new definition file for api

--- src/app/definitions/StackExchangeApi.d.ts
+++ src/app/definitions/StackExchangeApi.d.ts
@@ -1,41 +1,56 @@
-// ApiStackexchangeQuestions
-// generated from https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow
-// using http://rest-area.firebaseapp.com 
 export interface ApiStackexchangeQuestions {
-    items: ApiStackexchangeItems[];
-    has_more: boolean;
-    quota_max: number;
-    quota_remaining: number;
-  }
-  // ApiStackexchangeItems
-  export interface ApiStackexchangeItems {
-    tags: (string)[];
-    owner: ApiStackexchangeOwner;
-    is_answered: boolean;
-    view_count: number;
-    answer_count: number;
-    score: number;
-    last_activity_date: number;
-    creation_date: number;
-    last_edit_date?: number | null;
-    question_id: number;
-    link: string;
-    title: string;
-    closed_date?: number | null;
-    closed_reason?: string | null;
-    accepted_answer_id?: number | null;
-    body: string;
-  }
-  // ApiStackexchangeOwner
-  export interface ApiStackexchangeOwner {
-    reputation: number;
-    user_id: number;
-    user_type: string;
-    profile_image: string;
-    display_name: string;
-    link: string;
-    accept_rate?: number | null;
-  }
-  
-  
-  
+  items?: (ItemsEntity)[];
+  has_more: boolean;
+  quota_max: number;
+  quota_remaining: number;
+}
+export interface ApiStackexchangeItems {
+  tags?: (string)[];
+  comments?: (CommentsEntity)[] | null;
+  answers?: (AnswersEntity)[] | null;
+  owner: Owner;
+  is_answered: boolean;
+  view_count: number;
+  accepted_answer_id?: number | null;
+  answer_count: number;
+  score: number;
+  last_activity_date: number;
+  creation_date: number;
+  last_edit_date?: number | null;
+  question_id: number;
+  link: string;
+  title: string;
+  body: string;
+}
+export interface CommentsEntity {
+  owner: Owner;
+  edited: boolean;
+  score: number;
+  creation_date: number;
+  post_id: number;
+  comment_id: number;
+  body: string;
+  reply_to_user?: Owner | null;
+}
+export interface Owner {
+  reputation: number;
+  user_id: number;
+  user_type: string;
+  profile_image: string;
+  display_name: string;
+  link: string;
+  accept_rate?: number | null;
+}
+
+export interface AnswersEntity {
+  comments?: (CommentsEntity)[] | null;
+  owner: Owner;
+  is_accepted: boolean;
+  score: number;
+  last_activity_date: number;
+  creation_date: number;
+  answer_id: number;
+  question_id: number;
+  body: string;
+  last_edit_date?: number | null;
+}
