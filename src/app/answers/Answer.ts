02e69bfe2c62eb854a170f1fa346817a7ae57223
TheLox95 <leo_9_t@hotmail.com>
2018-02-18 09:36:23 -0400

    +entities defined

--- src/app/answers/Answer.ts
+++ src/app/answers/Answer.ts
@@ -1,16 +1,58 @@
 import { AnswersEntity, CommentsEntity, Owner } from "../definitions/StackExchangeApi";
-
+	
 export class Answer implements AnswersEntity{
-    comments?: CommentsEntity[];
-    owner: Owner;
-    is_accepted: boolean;
-    score: number;
-    last_activity_date: number;
-    creation_date: number;
-    answer_id: number;
-    question_id: number;
-    body: string;
-    last_edit_date?: number;
-    
+    private _comm]ents?: CommentsEntity[];
+    private _owner: Owner;
+    private _is_accepted: boolean;
+    private _score: number;
+    private _last_activity_date: number;
+    private _creation_date: number;
+    private _answer_id: number;
+    private _question_id: number;
+    private _body: string;
+    private _last_edit_date?: number;
+
+    constructor(owner: Owner, is_accepted: boolean, score: number, last_activity_date: number, creation_date: number, answer_id: number, question_id: number, body: string) {
+		this._owner = owner;
+		this._is_accepted = is_accepted;
+		this._score = score;
+		this._last_activity_date = last_activity_date;
+		this._creation_date = creation_date;
+		this._answer_id = answer_id;
+		this._question_id = question_id;
+		this._body = body;
+	}
+
+	public get owner(): Owner {
+		return this._owner;
+	}
+
+	public get is_accepted(): boolean {
+		return this._is_accepted;
+	}
+
+	public get score(): number {
+		return this._score;
+	}
+
+	public get last_activity_date(): number {
+		return this._last_activity_date;
+	}
+
+	public get creation_date(): number {
+		return this._creation_date;
+	}
+
+	public get answer_id(): number {
+		return this._answer_id;
+	}
+
+	public get question_id(): number {
+		return this._question_id;
+	}
+
+	public get body(): string {
+		return this._body;
+	}
 
 }
--- src/app/comment/Comment.ts
+++ src/app/comment/Comment.ts
@@ -1,13 +1,52 @@
 import { CommentsEntity, Owner } from "../definitions/StackExchangeApi";
 
 export class Comment implements CommentsEntity{
-    owner: Owner;
-    edited: boolean;
-    score: number;
-    creation_date: number;
-    post_id: number;
-    comment_id: number;
-    body: string;
-    reply_to_user?: Owner;
+	private _edited: boolean;
+    private _score: number;
+    private _creation_date: number;
+    private _post_id: number;
+    private _comment_id: number;
+    private _body: string;
+    private _reply_to_user?: Owner;
+
+	constructor(owner: Owner, edited: boolean, score: number, creation_date: number, post_id: number, comment_id: number, body: string) {
+		this._owner = owner;
+		this._edited = edited;
+		this._score = score;
+		this._creation_date = creation_date;
+		this._post_id = post_id;
+		this._comment_id = comment_id;
+		this._body = body;
+	}
+
+	public get owner(): Owner {
+		return this._owner;
+	}
+    private _owner: Owner;
+
+	public get edited(): boolean {
+		return this._edited;
+	}
+
+	public get score(): number {
+		return this._score;
+	}
+
+	public get creation_date(): number {
+		return this._creation_date;
+	}
+
+	public get post_id(): number {
+		return this._post_id;
+	}
+
+	public get comment_id(): number {
+		return this._comment_id;
+	}
+
+	public get body(): string {
+		return this._body;
+	}
+    
 
 }
