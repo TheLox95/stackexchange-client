07d06ecb226c795efd4283f8acf30932ec9507a2
TheLox95 <leo_9_t@hotmail.com>
2018-02-18 20:15:54 -0400

    +now question-list show a modal with the question and its comments

--- src/app/question/question-list/question-list.component.ts
+++ src/app/question/question-list/question-list.component.ts
@@ -1,41 +1,98 @@
-import {Router, ActivatedRoute} from '@angular/router';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { QuestionService } from '../question.service';
-import { Question } from '../Question';
-import { Observable } from 'rxjs/Observable';
-import { SuiModalService, TemplateModalConfig, ModalTemplate } from 'ng2-semantic-ui';
+import { Router, ActivatedRoute } from "@angular/router";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { QuestionService } from "../question.service";
+import { Question } from "../Question";
+import { Observable } from "rxjs/Observable";
+import {
+  SuiModalService,
+  TemplateModalConfig,
+  ModalTemplate
+} from "ng2-semantic-ui";
 
 export interface IContext {
   question: Question;
 }
 
 @Component({
-  selector: 'app-question-list',
+  selector: "app-question-list",
   template: `<p>
   question-list works!
 </p>
   <ul *ngFor="let questionObj of questions$ | async">
     <li><a [innerHTML]="questionObj.title" (click)="open(questionObj)"></a></li>
     <ng-template let-context let-modal="modal" #modalTemplate>
-    <div class="header" [innerHtml]="context.question.title"></div>
-    <div class="content">
-        <p [innerHtml]="context.question.body"></p>
+    <div class="header" [innerHtml]="context.question.title + ' asked by ' + context.question.owner.display_name" suiPopup [popupTemplate]="popupTemplate" popupTrigger="click"></div>
+    <sui-tabset>
+    <div class="ui top attached tabular menu">
+        <a class="item" suiTabHeader="1">Question</a>
+        <a class="item" suiTabHeader="2">Answers</a>
     </div>
+    <div class="ui bottom attached segment" suiTabContent="1">
+    <div class="content">
+        <div class="ui grid">
+          <div class="left floated six wide column" [innerHtml]="context.question.body"></div>
+          <div class="container six wide column" >
+
+              <div class="ui raised secondary piled segment right floated">
+              <sui-accordion [closeOthers]="false">
+                <sui-accordion-panel [isOpen]="true">
+                <div title>
+                    <i class="dropdown icon"></i>
+                    Comments
+                </div>
+                <div content>
+                <div class="ui comments" *ngFor="let comment of context.question.comments">
+                <div class="comment">
+                  <a class="avatar">
+                    <img src="{{comment.owner.profile_image}}">
+                  </a>
+                  <div class="content">
+                    <a class="author">{{comment.owner.display_name}}</a>
+                    <div class="metadata">
+                      <span class="date">{{comment.creation_date}}</span>
+                    </div>
+                    <div class="text" [innerHtml]="comment.body"></div>
+                  </div>
+                </div>
+
+
+                </div>
+                </div>
+                </sui-accordion-panel>
+                </sui-accordion>
+              </div>
+          </div>
+        </div>
+    </div>
+    </div>
+    <div class="ui bottom attached segment" suiTabContent="2">Second tab!</div>
+    </sui-tabset>
     <div class="actions">
         <button class="ui secondary button" (click)="modal.approve('approved')" autofocus>Close</button>
     </div>
+    <ng-template let-popup #popupTemplate>
+      <div class="image">
+        <img src="{{context.question.owner.profile_image}}">
+      </div>
+      <div class="content">
+        <a class="header" href="{{context.question.owner.link}}" [innerHtml]="context.question.owner.display_name"></a>
+      </div>
+    </ng-template>
 </ng-template>
 </ul>
 `,
-  styleUrls: ['./question-list.component.css']
+  styleUrls: ["./question-list.component.css"]
 })
 export class QuestionListComponent implements OnInit {
-  questions$ : Observable<Question[]>;
+  questions$: Observable<Question[]>;
   @ViewChild("modalTemplate")
   public modalTemplate: ModalTemplate<IContext, string, string>;
   _question;
 
-  constructor(private questionService: QuestionService, public modalService: SuiModalService) { }
+  constructor(
+    private questionService: QuestionService,
+    public modalService: SuiModalService
+  ) {}
 
   ngOnInit() {
     this.questions$ = this.questionService.getList();
@@ -47,11 +104,9 @@ export class QuestionListComponent implements OnInit {
     );
 
     config.closeResult = "closed!";
-    config.context = { question:  dynamicContent};
+    config.isFullScreen = true;
+    config.context = { question: dynamicContent };
 
-    this.modalService
-      .open(config);
+    this.modalService.open(config);
   }
-
-
 }
